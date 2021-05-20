06ffc333137211c0d8157d6f44b5d5edd211ed42
duckbot <84015647+duckbotweb@users.noreply.github.com>
2021-05-20 17:17:54 +0200

    Wow

---
+++ pages/reg.php
@@ -0,0 +1,10 @@
+<html>
+<head>
+    <?php
+    $regfiles = fopen("registernumbers.txt", "w")
+    $text = "just a random number, don't worry.";
+    fwrite($myfile, $text)
+    fclose($myfile)
+    ?>
+</head>
+</html>
